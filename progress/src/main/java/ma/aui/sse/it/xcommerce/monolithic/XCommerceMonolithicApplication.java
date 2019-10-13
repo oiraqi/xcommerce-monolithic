f2a76c4640f015b40be8bda92773c3b71f7336d9
Omar Iraqi <o.iraqi@gmail.com>
2019-10-13 19:13:54 +0100

    removed caching from initial and progress

--- progress/src/main/java/ma/aui/sse/it/xcommerce/monolithic/XCommerceMonolithicApplication.java
+++ progress/src/main/java/ma/aui/sse/it/xcommerce/monolithic/XCommerceMonolithicApplication.java
@@ -2,10 +2,8 @@ package ma.aui.sse.it.xcommerce.monolithic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class XCommerceMonolithicApplication {
 
 	public static void main(String[] args) {
