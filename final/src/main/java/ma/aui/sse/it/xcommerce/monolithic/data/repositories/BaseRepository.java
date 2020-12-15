3dff21ee6054395ce743011cbe6cb93b9b50f17d
Omar Iraqi <o.iraqi@gmail.com>
2020-12-15 23:30:49 +0100

    updates

--- final/src/main/java/ma/aui/sse/it/xcommerce/monolithic/data/repositories/BaseRepository.java
+++ final/src/main/java/ma/aui/sse/it/xcommerce/monolithic/data/repositories/BaseRepository.java
@@ -1,9 +1,5 @@
 package ma.aui.sse.it.xcommerce.monolithic.data.repositories;
 
-/*import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.Query;*/
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,9 +9,5 @@ import ma.aui.sse.it.xcommerce.monolithic.data.entities.BaseEntity;
 @CrossOrigin(origins = "http://localhost:4200")
 @NoRepositoryBean
 public interface BaseRepository<E extends BaseEntity> extends CrudRepository<E, Long> {
-    /*@Query("select e from E e where e.id = ?1 and e.active = true")
-    Optional<E> findById(String name);
-
-    @Query("select e from E e where e.active = false")
-    List<E> findInactive();*/
+    
 }
