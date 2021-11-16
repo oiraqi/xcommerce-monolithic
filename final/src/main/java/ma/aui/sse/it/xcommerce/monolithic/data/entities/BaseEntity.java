22cefba74815a20847e74cdd795317c813358402
Omar Iraqi <o.iraqi@gmail.com>
2021-11-16 08:39:06 +0100

    updates

--- final/src/main/java/ma/aui/sse/it/xcommerce/monolithic/data/entities/BaseEntity.java
+++ final/src/main/java/ma/aui/sse/it/xcommerce/monolithic/data/entities/BaseEntity.java
@@ -1,9 +1,11 @@
 package ma.aui.sse.it.xcommerce.monolithic.data.entities;
 
-import org.springframework.data.jpa.domain.AbstractAuditable;
+// import org.springframework.data.jpa.domain.AbstractAuditable;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 /**
@@ -12,9 +14,21 @@ import javax.persistence.MappedSuperclass;
  */
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity extends AbstractAuditable<User,Long> {
+public abstract class BaseEntity /*extends AbstractAuditable<User,Long>*/ {
 
     private static final long serialVersionUID = -1152317130147556701L;
+
+    @GeneratedValue
+    @Id
+    protected Long id;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     protected boolean active = true;
 
